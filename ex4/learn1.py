09669d3c839f496b02d3ebbe967416b2e83f3e5e
Oshima Uta <e245726@ie.u-ryukyu.ac.jp>
2026-04-28 15:28:28 +0900

    1.11

--- ex4/learn1.py
+++ ex4/learn1.py
@@ -1,6 +1,8 @@
-from dataset1 import load_dataset_from_tsv
+from dataset1 import load_dataset_from_tsv, true_function
 import regression
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 # データ読み込み
@@ -24,9 +26,24 @@ import regression
 model = regression.LinearRegression()
 model.fit(x_train, y_train)
 
-print(model.theta)
+#print(model.theta)
 
 # 予測
 y_pred = model.predict(x_test)
 
-print(y_pred)
+#print(y_pred)
+
+x = np.linspace(-1, 1, 100)
+y = true_function(x)
+x_plot = np.linspace(-1, 1, 100).reshape(-1, 1)
+y_pred = model.predict(x_plot)
+
+plt.plot(x, y, label="y = sin(pi * 0.8 * x) * 10")
+plt.scatter(df["観測点"], df["観測値"], label="Observed (Noisy)", alpha=0.7)
+plt.scatter(x_plot, y_pred, label="predicted", alpha=0.7)
+plt.legend()
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("True Function")
+plt.savefig("ex4/ex1.10.png")
+plt.show()
