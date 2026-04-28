552536428d20b2880d3c33ddf9f84f329488751a
Oshima Uta <e245726@ie.u-ryukyu.ac.jp>
2026-04-28 09:29:57 +0900

    ver2

--- week3/test.py
+++ week3/test.py
@@ -1,5 +1,14 @@
 import datasets
+import numpy as np
 X,Y = datasets.load_linear_example1()
 print(X)
 print(X[0])
 print(Y)
+
+Z= np.dot(Y,X)
+print(Z)
+
+import regression
+model = regression.LinearRegression()
+model.fit(X,Y)
+print(model.theta)
